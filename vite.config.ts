7e84fdf1d33f476e0719e5fb5019e19ec8f0d039
Peter Kepes <kepesp@gmail.com>
2026-07-11 15:07:12 +0200

    Open browser automatically when dev/preview server starts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  server: {
+    open: true,
+  },
+  preview: {
+    open: true,
+  },
   test: {
     environment: 'jsdom',
     globals: true,
